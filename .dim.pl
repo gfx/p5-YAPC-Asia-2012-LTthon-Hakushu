be62e7f0f032f10e4c62e23d419db52265a647c7
Fuji, Goro <g.psy.va@gmail.com>
2012-09-28 12:26:00 +0900

    initial commit

---
+++ .dim.pl
@@ -0,0 +1,7 @@
+# This file is managed by /Users/gfx/perl5/perlbrew/perls/perl-5.16.1/bin/dim.
+{
+  dist => 'Acme::YAPC::Asia::2012::LTthon::Hakushu',
+  distdir => 'Acme-YAPC-Asia-2012-LTthon-Hakushu',
+  module => 'Acme::YAPC::Asia::2012::LTthon::Hakushu',
+  template => 'Default'
+}
