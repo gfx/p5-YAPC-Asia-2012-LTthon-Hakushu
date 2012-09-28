8e7eb8ee45c6e3f318979422e6f77ff0d7e448d0
Fuji, Goro <g.psy.va@gmail.com>
2012-09-28 12:26:27 +0900

    remove .dim.pl

--- .dim.pl
+++
@@ -1,7 +0,0 @@
-# This file is managed by /Users/gfx/perl5/perlbrew/perls/perl-5.16.1/bin/dim.
-{
-  dist => 'Acme::YAPC::Asia::2012::LTthon::Hakushu',
-  distdir => 'Acme-YAPC-Asia-2012-LTthon-Hakushu',
-  module => 'Acme::YAPC::Asia::2012::LTthon::Hakushu',
-  template => 'Default'
-}
